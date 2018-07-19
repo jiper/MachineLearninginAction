433bef41234bb5e066d3d3914e93300269d46160
jiper <542062320@qq.com>
2018-07-19 10:00:40 +0800

    第一次提交

---
+++ Ch02/kNN.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 15:39:38 2018
+
+@author: JianLinpeng
+"""
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array ([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group,labels
